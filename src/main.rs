5945fd9a4eeafb4dcac2734238f2a1672e53bb38
Alexander <akrimec@gmail.com>
2024-11-27 14:28:15 +0200

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+fn next_language<'a>(languages: &'a [String], current: &str) -> &'a str {
+    let mut found = false;
+
+    for language in languages {
+        if found {
+            return language;
+        }
+        if language == current {
+            found = true;
+        }
+    }
+
+    languages.last().unwrap()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let languages = vec![
+        String::from("rust"),
+        String::from("python"),
+        String::from("sh"),
+    ];
+
+    let result = next_language(&languages, "python");
+    println!("{:?}", result);
 }
